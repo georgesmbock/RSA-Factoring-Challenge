93e9759583b78cc1b86b833a63250d3bbfc262b6
georgesmbock <mbock@DESKTOP-3KEOBHU>
2024-04-08 03:19:53 +0100

    second commit

--- factors.py
+++ factors.py
@@ -2,30 +2,33 @@
 import sys
 import time
 
-#n = 2
+"""n = 2
 if len(sys.argv) == 1:
     print("Add argument")
     print("\tusage: python3 add.py file")
     exit()
+"""
 file = sys.argv[1]
 
-with open(file, "r") as f:
-    line_str = f.readline()
-    while line_str:
-        n = 2
-        line_int = int(line_str)
-        while (line_int % n) == 0:
+f= open(file, "r")
+    #line_str = f.readline()
+for line_str in f:
+    n = 2
+    line_int = int(line_str)
+    reste_modulo = (line_int % n)
+    while reste_modulo == 0:
+        r = line_int // n
+    #if (line_int % n == 0):
+        #r = line_int // n
+        print(f"{line_int}={r}*{n}")
+        break
+            #n += 1
+        #continue
+    while reste_modulo != 0:
+        n +=1
+        if (line_int % n == 0):
             r = line_int // n
-            if (line_int % n == 0):
-                print(f"{line_int}={r}*{n}")
-                break
-            n += 1
-            continue
-        while (line_int % n) != 0:
-            n +=1
-            if (line_int % n == 0):
-                r = line_int // n
-                print(f"{line_int}={r}*{n}")
-                break
-        line_str = f.readline()
-    f.close()
+            print(f"{line_int}={r}*{n}")
+            break
+        #line_str = f.readline()
+f.close()
